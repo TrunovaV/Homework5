b7c03f5a7ca0f3c94fa4cff4a2c737ab3bd3729b
Viktoria <viki.panterka@yandex.ru>
2026-06-09 17:34:02 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,29 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
+        System.out.println("Домашнее задание 5");
+        System.out.println("Задача 1 и 2");
 
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1){
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
+        System.out.println("Задача 2");
+        int clientOS1 = 1;
+        int yearOfProduction = 2014;
+        if (clientOS1 == 0 && yearOfProduction >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS1 == 0 && yearOfProduction < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS1 ==1 && yearOfProduction >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS1 == 1 && yearOfProduction < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        System.out.println("задача 3");
+
     }
 }
