8c819749bcb57bb44ec44807a3e995f5cd1af271
Viktoria <viki.panterka@yandex.ru>
2026-06-12 13:34:55 +0300

    ДЗ выполнено

--- src/Main.java
+++ src/Main.java
@@ -29,6 +29,52 @@ public class Main {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
         System.out.println("задача 3");
+        int year = 2020;
+        if (year > 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является высокосным");
+        } else {
+            System.out.println(year + " год не является высокосным");
+        }
+        System.out.println("Задача 4");
+        int deliveryDistance = 95;
+        int deliveryDays = 1;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + deliveryDays);
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            deliveryDays += 1;
+            System.out.println("потребуется дней: " + deliveryDays);
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            deliveryDays += 2;
+            System.out.println("Потребуется дней " + deliveryDays);
+        } else {
+            System.out.println("Доставки нет");
+        }
+        System.out.println("Задача 5");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+        }
 
     }
 }
