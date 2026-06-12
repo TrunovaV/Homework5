bd3a7767c2ddc4ebc6199c035ff1807d31317080
Viktoria <viki.panterka@yandex.ru>
2026-06-12 22:27:09 +0300

    ДЗ - исправленное 2

--- src/Main.java
+++ src/Main.java
@@ -40,10 +40,12 @@ public class Main {
         int deliveryDays = 1;
         if (deliveryDistance < 20) {
             System.out.println("Потребуется дней: " + deliveryDays);
-        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+        }
+        if (deliveryDistance >= 20 && deliveryDistance < 60) {
             deliveryDays += 1;
             System.out.println("потребуется дней: " + deliveryDays);
-        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+        }
+        if (deliveryDistance >= 60 && deliveryDistance <= 100) {
             deliveryDays += 2;
             System.out.println("Потребуется дней " + deliveryDays);
         } else {
